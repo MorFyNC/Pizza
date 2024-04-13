02ae2fe8d1153c62f53ebc8366793f6cdbee0a8e
212203 <212203@1220-009-22>
2024-04-13 09:26:08 +0300

    Добавьте файлы проекта.

---
+++ BlazorApp1/Data/Cart.cs
@@ -0,0 +1,20 @@
+﻿namespace BlazorApp1.Data
+{
+    public class Cart
+    {
+        public Dictionary<Pizza, int> pizzas = new Dictionary<Pizza, int>();
+        public int Summary;
+
+        public void Add(Pizza pizza)
+        {
+            if(pizzas.ContainsKey(pizza))
+            {
+                pizzas[pizza] += 1;
+            }
+            else
+            {
+                pizzas.Add(pizza, 1);
+            }
+        }
+    }
+}
---
+++ BlazorApp1/Data/Pizza.cs
@@ -0,0 +1,18 @@
+﻿namespace BlazorApp1.Data
+{
+    public class Pizza
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Cost { get; set; }
+        public string Image { get; set; }
+
+        public Pizza(string name, string description, int cost, string image)
+        {
+            Name = name;
+            Description = description;
+            Cost = cost;
+            Image = image;
+        }
+    }
+}
